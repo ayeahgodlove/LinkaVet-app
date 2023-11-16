36e85be992b6b669bb75cfeb879bcf6d1e45ee90
Ayeah Godlove A <ayeahgodlove5@gmail.com>
2023-11-16 14:04:17 +0100

    update details page

--- src/Pages/product/product-detail.page.tsx
+++ src/Pages/product/product-detail.page.tsx
@@ -1,15 +1,16 @@
-import { Col, Row } from "antd";
-import ProductDetailComponent from "components/admin/product/product-detail.component";
-import ProductBannerComponent, {
-  ProductBanner,
-} from "components/product/product-banner.component";
+import { Col, Divider, Row, Typography } from "antd";
+import { ProductBanner } from "components/product/product-banner.component";
+import GridView from "components/product/product-card-grid.component";
 import ProductDetail from "components/product/product-detail.component";
 import BackButton from "components/shared/back-button.component";
+import { NoContent } from "components/shared/no-content/no-content.component";
 import PageBreadCrumbs from "components/shared/page-breadcrumb/page-breadcrumb.component";
+import { useProduct } from "hooks/product.hook";
 import GeneralAppShell from "layout/app/general-app-shell";
 import React from "react";
 
 const ProductDetailPage: React.FC = () => {
+  const { products } = useProduct();
   return (
     <GeneralAppShell>
       <ProductBanner />
@@ -24,6 +25,17 @@ const ProductDetailPage: React.FC = () => {
           <ProductDetail />
         </Col>
       </Row>
+      <Col span={24} style={{ marginTop: 30 }}>
+        <Typography.Title level={3} style={{ textAlign: "center" }}>
+          You may also like
+        </Typography.Title>
+        <Divider style={{ marginTop: 0 }} />
+      </Col>
+      {products && products.length > 0 ? (
+        <GridView products={products} resultProducts={[]} />
+      ) : (
+        <NoContent title="Your shopping cart is empty at the moment" />
+      )}
     </GeneralAppShell>
   );
 };
