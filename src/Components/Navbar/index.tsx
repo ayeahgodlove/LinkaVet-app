5c9f9856cfda994a1af1713ed12e940b9306ce74
Ayeah Chanser <ayeahchanser@gmail.com>
2023-11-23 20:27:57 +0100

    update: run build command

--- src/Components/Navbar/index.tsx
+++ src/Components/Navbar/index.tsx
@@ -7,6 +7,7 @@ import RightMenu from "./right-menu";
 import { Header } from "antd/es/layout/layout";
 import { useTheme } from "hooks/shared/theme.hook";
 import "./navbar.style.scss";
+import useWindowSize from "hooks/shared/window-resize.hook";
 
 interface IProps {
   showMenuIcon?: boolean;
@@ -15,30 +16,31 @@ interface IProps {
 
 const Navbar: React.FC<IProps> = ({ showMenuIcon = false, handleShow }) => {
   const { isDarkMode } = useTheme();
+  const { width } = useWindowSize();
   return (
     <ConfigProvider
-    theme={{
-      token: {
-        colorPrimary: "#08a30a",
-        colorLink: "#214e0a",
-        fontFamily: "Poppins",
-        fontSize: 16,
-        fontWeightStrong: 500,
-      },
-    }} 
-  >
+      theme={{
+        token: {
+          colorPrimary: "#08a30a",
+          colorLink: "#214e0a",
+          fontFamily: "Poppins",
+          fontSize: 16,
+          fontWeightStrong: 500,
+        },
+      }}
+    >
       <Header
-        style={{ background: isDarkMode ? "" : "#fff", position: "sticky" }}
+        style={{ background: isDarkMode ? "" : "#fff", position: "sticky"}}
         className="menuBar header-box-shadow"
       >
         <div className="logo">
-          <Link to="/" style={{ marginLeft: 1 }}>
+          <Link to="/">
             <img
               src="/logo/logo-2-removebg-preview.png"
-              height={100}
-              width={90}
+              height={width > 768 ? 100 : 90}
+              width={width > 768 ? 90 : 60}
               alt="honeyman logo"
-              style={{ aspectRatio: 1/1, objectFit: "contain"}}
+              style={{ aspectRatio: 1 / 1, objectFit: "contain" }}
             />
           </Link>
         </div>
